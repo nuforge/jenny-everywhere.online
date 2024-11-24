27b14d25a77bd3d89e6e7203052dd2681bb8176c
nuForge <nuforge@gmail.com>
2024-11-23 22:09:08 -0500

    test markdown support with 'marked' js library

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('Hello World! 😁')
+
+function fetch_story() {
+    let storyHTML = ''
+    fetch("story.md")
+        .then((result) => result.text())
+        .then((text) => {
+            storyHTML = marked.parse(text)
+            document.getElementById('story-body').innerHTML = storyHTML
+            console.log(storyHTML)
+        })
+        .catch((e) => console.error(e));
+}
